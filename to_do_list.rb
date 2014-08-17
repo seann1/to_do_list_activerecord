9b79df7ed1bcedd348f2a920e495e3c3ab2831b0
Sean <student@epicodus.com>
2014-08-16 17:50:35 -0700

    add spec helper, Task class and ui files

---
+++ to_do_list.rb
@@ -0,0 +1,6 @@
+require 'active_record'
+require './lib/task'
+
+database_configurations = YAML::load(File.open('./db/config.yml'))
+development_configuration = database_configurations['development']
+ActiveRecord::Base.establish_connection(development_configuration)
